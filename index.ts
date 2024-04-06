fc051500b0d1bdd6a2168416ee8292981f370221
Ashfaque Ali Joyo <ashfaque7813@gmail.com>
2024-04-06 09:58:37 -0700

    push my number guessing to github

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+// 1) Computer will generate a random number
+// 2) User input for guessing number
+// 3) Compare user input with computer generated number and show result
+const randonNumber = Math.floor(Math.random() * 8 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGussedNumber",
+        type: "number",
+        message: "You can guess a number between 1-8: ",
+    },
+]);
+if(answers.userGussedNumber === randonNumber){
+    console.log("Hurry! you guessed right number.");
+}else{
+    console.log("Sorry! You guessed wrong number.");
+}
